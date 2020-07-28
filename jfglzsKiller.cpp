7e5470a52598d7ecb8a5c431d506aac1e3b1a047
赵崇延 <47052088+ZhaoChongyan@users.noreply.github.com>
2020-07-28 15:01:36 +0800

    Add files via upload

--- jfglzsKiller.cpp
+++ jfglzsKiller.cpp
@@ -1,41 +1,40 @@
 #include <cstdlib>
 #include <windows.h>
 #include <TlHelp32.h>
-#include <iostream>
 #include <queue>
 using namespace std;
 
 int main(void) {
-	queue<HANDLE> ProList;
 	HANDLE hSnapshort=CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
 	if(hSnapshort==INVALID_HANDLE_VALUE) {
-		printf("CreateToolhelp32Snapshot failed!\n");
+		throw "CreateToolhelp32Snapshot failed!\n";
 		return -1;
 	}
+	queue<HANDLE> proList;
 	begin:;
 	PROCESSENTRY32 stcProcessInfo;
 	stcProcessInfo.dwSize=sizeof(stcProcessInfo);
 	BOOL bRet=Process32First(hSnapshort,&stcProcessInfo);
 	while(bRet) {
-		if(!(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
-		* strcmp(stcProcessInfo.szExeFile,"prozs.exe")
-		* strcmp(stcProcessInfo.szExeFile,"srvany.exe")
-		* strcmp(stcProcessInfo.szExeFile,"zmservice.exe"))) {
-			ProList.push(::OpenProcess(PROCESS_TERMINATE,FALSE,stcProcessInfo.th32ProcessID));
+		if(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"prozs.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"srvany.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"zmservice.exe")) {
+			proList.push(::OpenProcess(PROCESS_TERMINATE,FALSE,stcProcessInfo.th32ProcessID));
 		}
 		bRet=Process32Next(hSnapshort,&stcProcessInfo);
 	}
-	while(ProList.size()){
-		::TerminateProcess(ProList.front(),0);
-		CloseHandle(ProList.front());
-		ProList.pop();
+	while(proList.size()){
+		::TerminateProcess(proList.front(),0);
+		CloseHandle(proList.front());
+		proList.pop();
 	}
 	bRet=Process32First(hSnapshort,&stcProcessInfo);
 	while(bRet) {
-		if(!(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
-		* strcmp(stcProcessInfo.szExeFile,"prozs.exe")
-		* strcmp(stcProcessInfo.szExeFile,"srvany.exe")
-		* strcmp(stcProcessInfo.szExeFile,"zmservice.exe"))) {
+		if(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"prozs.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"srvany.exe")
+		|| strcmp(stcProcessInfo.szExeFile,"zmservice.exe")) {
 			goto begin;
 		}
 		bRet=Process32Next(hSnapshort,&stcProcessInfo);
