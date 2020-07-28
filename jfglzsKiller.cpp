0b257ce5c81aaba0e03f1820490f6da73ffb5aa5
赵崇延 <47052088+ZhaoChongyan@users.noreply.github.com>
2020-07-28 15:41:16 +0800

    Add files via upload

--- jfglzsKiller.cpp
+++ jfglzsKiller.cpp
@@ -5,40 +5,43 @@
 using namespace std;
 
 int main(void) {
-	HANDLE hSnapshort=CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
-	if(hSnapshort==INVALID_HANDLE_VALUE) {
+	HANDLE hSnapshort = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshort == INVALID_HANDLE_VALUE) {
 		throw "CreateToolhelp32Snapshot failed!\n";
 		return -1;
 	}
 	queue<HANDLE> proList;
-	begin:;
-	PROCESSENTRY32 stcProcessInfo;
-	stcProcessInfo.dwSize=sizeof(stcProcessInfo);
-	BOOL bRet=Process32First(hSnapshort,&stcProcessInfo);
-	while(bRet) {
-		if(!(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"prozs.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"srvany.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"zmservice.exe"))) {
-			proList.push(::OpenProcess(PROCESS_TERMINATE,FALSE,stcProcessInfo.th32ProcessID));
+	bool finished = false; 
+	do {
+		finished = true;
+		PROCESSENTRY32 stcProcessInfo;
+		stcProcessInfo.dwSize = sizeof(stcProcessInfo);
+		BOOL bRet = Process32First(hSnapshort, &stcProcessInfo);
+		while(bRet) {
+			if(!(strcmp(stcProcessInfo.szExeFile, "jfglzs.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "prozs.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "srvany.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "zmservice.exe"))) {
+				proList.push(::OpenProcess(PROCESS_TERMINATE, FALSE, stcProcessInfo.th32ProcessID));
+			}
+			bRet = Process32Next(hSnapshort, &stcProcessInfo);
 		}
-		bRet=Process32Next(hSnapshort,&stcProcessInfo);
-	}
-	while(proList.size()){
-		::TerminateProcess(proList.front(),0);
-		CloseHandle(proList.front());
-		proList.pop();
-	}
-	bRet=Process32First(hSnapshort,&stcProcessInfo);
-	while(bRet) {
-		if(!(strcmp(stcProcessInfo.szExeFile,"jfglzs.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"prozs.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"srvany.exe")
-		&& strcmp(stcProcessInfo.szExeFile,"zmservice.exe"))) {
-			goto begin;
+		while (proList.size()) {
+			::TerminateProcess(proList.front(), 0);
+			CloseHandle(proList.front());
+			proList.pop();
 		}
-		bRet=Process32Next(hSnapshort,&stcProcessInfo);
-	}
+		bRet = Process32First(hSnapshort, &stcProcessInfo);
+		while (bRet) {
+			if(!(strcmp(stcProcessInfo.szExeFile, "jfglzs.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "prozs.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "srvany.exe")
+			        && strcmp(stcProcessInfo.szExeFile, "zmservice.exe"))) {
+			    finished = false;
+			}
+			bRet = Process32Next(hSnapshort, &stcProcessInfo);
+		}
+	} while(!finished);
 	CloseHandle(hSnapshort);
 	return 0;
 }
